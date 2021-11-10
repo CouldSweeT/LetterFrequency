7c65ef963f90f24573193da34a582db9b6b731e8
Igor Igor <you@example.com>
2021-11-10 19:45:10 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Fragment, useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
+interface LetterObject {
+  [keys : string]: number
+}
+
+export const App: React.FC = () => {
+  const [inputText, setInputText] = useState<string>('')
+  const [objectOfLetters, setObjectOfLetters] = useState<LetterObject>({})
+  const [sortBy, setSortBy] = useState<string>('')
+
+  useEffect(() => {
+    const objectLetters: LetterObject = {}
+
+    for (let letter of inputText) {
+      if (!objectLetters[letter]) {
+        objectLetters[letter] = 1
+      } else {
+        objectLetters[letter] += 1;
+      }
+    }
+
+    if(sortBy) {
+      let ordered = Object.keys(objectLetters).sort((a, b) => {
+        switch (sortBy) {
+          case 'letter':
+            return a.localeCompare(b);
+          case 'count':
+            return objectLetters[a] - objectLetters[b]
+          default:
+            return 0;
+        }
+      }).reduce(
+        (obj, key) => {
+          // @ts-ignore
+          obj[key] = objectLetters[key];
+          return obj;
+        },
+        {}
+      );
+
+      return setObjectOfLetters(ordered)
+    }
+
+    setObjectOfLetters(objectLetters)
+  }, [sortBy, inputText])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+     <input type="text" value={inputText} onChange={event => {
+       setInputText(event.target.value);
+     }}/>
+     <table>
+       <tr>
+         <th onClick={() => setSortBy('letter')}>Letter</th>
+         <th onClick={() => setSortBy('count')}>Count</th>
+         <th>%</th>
+       </tr>
+       {
+         Object.keys(objectOfLetters).map((letter: string)=>{
+
+           return (
+             <Fragment key={letter}>
+               <tr>
+                 <td>
+                   {letter}
+                 </td>
+                 <td>
+                   {objectOfLetters[letter]}
+                 </td>
+                 <td>
+                   {(objectOfLetters[letter] / Object.values(objectOfLetters).reduce((a, b) => a + b, 0) * 100).toFixed(2)}
+                 </td>
+               </tr>
+             </Fragment>
+           )
+         })
+       }
+     </table>
+   </>
+
   );
 }
 
